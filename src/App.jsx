fedb6cedd1fe9c6691164c2aed9f1eaac7fe95d2
Hxrt-1002 <m.ninlakan1@gmail.com>
2025-11-12 00:56:33 -0800

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <nav className='bg-gray-800 p-4'>
+        <ul className='flex space-x-4'>
+
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
