4fea4a570c55a457a43e0c8329200bb0037d5c61
Hxrt-1002 <jirapatlimcharoen@gmail.com>
2025-11-12 01:30:09 -0800

    gogogogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router , Routes , Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/contact'
 
 const App = () => {
   return (
@@ -8,12 +11,20 @@ const App = () => {
         <nav className='bg-gray-800 p-4'>
           <ul className='flex space-x-4'>
             <li>
-
+              <Link to="/Home" className='text-white hover:text-gray-300'>Home</Link>
+            </li>
+            <li>
+              <Link to="/About" className='text-white hover:text-gray-300'>About</Link>
+            </li>
+            <li>
+              <Link to="/Contact" className='text-white hover:text-gray-300'>Contact</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route />
+          <Route path='/Home' element={<Home/>}/>
+          <Route path='/About' element={<About/>}/>
+          <Route path='/Contact' element={<Contact/>}/>
         </Routes>
       </Router>
     </>
